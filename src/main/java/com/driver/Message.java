922e36389ba4e737faf5f42ed3f23f9cfe4ef21f
sanvu08 <sarveshkr1308@gmail.com>
2023-01-28 16:33:51 +0530

    Third Solution

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,13 +7,12 @@ public class Message {
     private String content;
     private Date timestamp;
 
-    public Message(int id, String content, Date timestamp) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = timestamp;
+    public Message() {
     }
 
-    public Message() {
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
     }
 
     public int getId() {
